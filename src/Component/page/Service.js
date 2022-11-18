b6a120ca748cb0907abaa5b7facfcad2a780b43d
obaidul islam1 <mursalinislam537215@gmail.com>
2022-11-18 20:32:23 +0600

    service data access complete

--- src/Component/page/Service.js
+++ src/Component/page/Service.js
@@ -1,7 +1,12 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 
 const Service = () => {
-    
+    const [services,setServices] = useState([]);
+    useEffect( () =>{
+       fetch('product.json') 
+       .then(res => res.json())
+       .then(data => setServices(data))
+    },[])
     return (
         <div>
             <div>
@@ -9,7 +14,7 @@ const Service = () => {
             <p className='text-center font-bold'>Fom leafy greens to cruciferous veggies, vegetables are a gift from nature. <br /> Filled with immune-boosting antioxidants, fiber, B-vitamins and minerals, <br /> vegetables can make a big impact on your health.</p>
             </div>
             <div className='mx-auto'>
-                this service is available
+               {services.length} 
             </div>
         </div>
     );
