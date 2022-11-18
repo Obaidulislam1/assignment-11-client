cec2e4d2fe96e5e8de993b064dc9e413759aab7c
obaidul islam1 <mursalinislam537215@gmail.com>
2022-11-18 11:57:00 +0600

    slider complete

---
+++ src/Component/page/Service.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Service = () => {
+    return (
+        <div>
+            <div>
+            <h1 className='text-center text-3xl font-bold'>Our best product</h1>
+            <p className='text-center font-bold'>Fom leafy greens to cruciferous veggies, vegetables are a gift from nature. <br /> Filled with immune-boosting antioxidants, fiber, B-vitamins and minerals, <br /> vegetables can make a big impact on your health.</p>
+            </div>
+            <div className='mx-auto'>
+                this service is available
+            </div>
+        </div>
+    );
+};
+
+export default Service;
