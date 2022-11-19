7fbd99203e863527eb21e7c26a9f20997c5d2f28
obaidul islam1 <mursalinislam537215@gmail.com>
2022-11-19 20:06:57 +0600

    firebase set up

--- src/Component/page/Login.js
+++ src/Component/page/Login.js
@@ -1,36 +1,45 @@
 import React from 'react';
 
 const Login = () => {
+
+    const handleClick = event =>{
+        event.preventDefault()
+        const form = event.target;
+        const email =form.email.value;
+        const password = form.password.value;
+        console.log(email,password);
+    }
     return (
         <div className="hero mt-5 mb-5">
             <div className="hero-content grid md:grid-cols-2 flex-col lg:flex-row">
                 <div className="text-center lg:text-left mr-5">
-                    <h1 className="text-5xl font-bold">Login now!</h1>
+                    <h1 className="text-5xl font-bold">Login now</h1>
                     <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi <br /> exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
                 </div>
-                <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+                <form onClick={handleClick} className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
                     <div className="card-body">
                         <h1 className='text-center text-3xl font-bold'>Login</h1>
                         <div className="form-control">
                             <label className="label">
                                 <span className="label-text">Email</span>
                             </label>
-                            <input type="text" placeholder="email" className="input input-bordered" />
+                            <input type="email" name='email' placeholder="email" className="input input-bordered" />
                         </div>
                         <div className="form-control">
                             <label className="label">
                                 <span className="label-text">Password</span>
                             </label>
-                            <input type="text" placeholder="password" className="input input-bordered" />
+                            <input name='password' type="password" placeholder="password" className="input input-bordered" />
                             <label className="label">
                                 <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
                             </label>
                         </div>
                         <div className="form-control mt-6">
-                            <button className="btn btn-primary">Login</button>
+                            <input type="submit" value="Login" className="btn btn-primary"/>
                         </div>
                     </div>
-                </div>
+                </form>
+
             </div>
         </div>
     );
