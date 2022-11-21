ee5d7c7b7b1ef25ef722a294f1f7888b92bbacbd
obaidul islam1 <mursalinislam537215@gmail.com>
2022-11-21 18:16:47 +0600

    review table running

--- src/Component/page/UserReview.js
+++ src/Component/page/UserReview.js
@@ -7,50 +7,34 @@ const UserReview = () => {
     const { user } = useContext(AuthContext)
 
     useEffect(() => {
-        fetch(`http://localhost:5000/review?email=${user.email}`)
+        fetch(`http://localhost:5000/review?email=${user?.email}`)
             .then(res => res.json())
             .then(data => setReview(data))
     }, [user?.email])
 
     return (
-        <div>
-             {
-                reviews.map(review => console.log(review.length))
-             }
-             <div className="overflow-x-auto">
-  <table className="table w-full">
-    <thead>
-      <tr>
-        <th></th>
-        <th>Name</th>
-        <th>Job</th>
-        <th>Favorite Color</th>
-      </tr>
-    </thead>
-    <tbody>
-      <tr>
-        <th>1</th>
-        <td>Cy Ganderton</td>
-        <td>Quality Control Specialist</td>
-        <td>Blue</td>
-      </tr>
-      <tr>
-        <th>2</th>
-        <td>Hart Hagerty</td>
-        <td>Desktop Support Technician</td>
-        <td>Purple</td>
-      </tr>
-      <tr>
-        <th>3</th>
-        <td>Brice Swyre</td>
-        <td>Tax Accountant</td>
-        <td>Red</td>
-      </tr>
-    </tbody>
-  </table>
-</div>
+        <div className="overflow-x-auto">
+            <table className="table w-full">
+                <thead>
+                    <tr>
+                        <th></th>
+                        <th>Gmail</th>
+                        <th>Item</th>
+                        <th>Review</th>
+                    </tr>
+                </thead>
+                <tbody>
+                {
+                        reviews.map(review => <UserReviewCard
+                        key={review._id}
+                        review={review}
+                        ></UserReviewCard>)
+                    }
+                </tbody>
+            </table>
         </div>
     );
 };
 
 export default UserReview;
+
---
+++ src/Component/page/UserReviewCard.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const UserReviewCard = ({ review }) => {
+    console.log(review);
+    return (
+        <>
+            <tr>
+                <th>1</th>
+                <td></td>
+                <td>Quality Control Specialist</td>
+                <td>Blue</td>
+            </tr>
+        </>
+    );
+};
+
+export default UserReviewCard;
