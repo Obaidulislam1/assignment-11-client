fbef91f1ca8b71f7368998c9984e6d6263937241
obaidul islam1 <mursalinislam537215@gmail.com>
2022-11-22 01:02:35 +0600

    problem solve

--- src/Component/page/UserReview.js
+++ src/Component/page/UserReview.js
@@ -12,6 +12,24 @@ const UserReview = () => {
             .then(data => setReview(data))
     }, [user?.email])
 
+    const handleDelete = id => {
+        const proceed = window.confirm('are you sure you want to cancel this order')
+        if (proceed) {
+            fetch(`http://localhost:5000/review/${id}`,{
+                method: 'DELETE',
+            })
+            .then(res => res.json())
+            .then(data =>{
+                console.log(data)
+                if(data.deletedCount > 0){
+                       alert('delete successfully');
+                       const remaining = reviews.filter(review => review._id !== id);
+                       setReview(remaining);
+                }
+            })
+        }
+    }
+
     return (
         <div className="overflow-x-auto">
             <table className="table w-full">
@@ -28,6 +46,7 @@ const UserReview = () => {
                         reviews.map(review => <UserReviewCard
                             key={review._id}
                             review={review}
+                            handleDelete={handleDelete}
                         ></UserReviewCard>)
                     }
                 </tbody>
--- src/Component/page/UserReviewCard.js
+++ src/Component/page/UserReviewCard.js
@@ -1,20 +1,9 @@
 import React from 'react';
 
-const UserReviewCard = ({ review }) => {
+const UserReviewCard = ({ review,handleDelete }) => {
     const { _id, email, name } = review;
 
-    const handleDelete = id => {
-        const proceed = window.confirm('are you sure you want to cancel this order')
-        if (proceed) {
-            fetch(`http://localhost:5000/review/${id}`,{
-                method: 'DELETE',
-            })
-            .then(res => res.json())
-            .then(data =>{
-                console.log(data)
-            })
-        }
-    }
+   
     return (
         <>
             <tr>
