364b94bd2f57357093f37521f1b5829a24201e88
obaidul islam1 <mursalinislam537215@gmail.com>
2022-11-21 17:10:06 +0600

    something wrong

--- src/Component/page/UserReview.js
+++ src/Component/page/UserReview.js
@@ -15,11 +15,40 @@ const UserReview = () => {
     return (
         <div>
              {
-                reviews.map(review => <UserReviewCard
-                key={review._id}
-                review={review}
-                ></UserReviewCard>)
+                reviews.map(review => console.log(review.length))
              }
+             <div className="overflow-x-auto">
+  <table className="table w-full">
+    <thead>
+      <tr>
+        <th></th>
+        <th>Name</th>
+        <th>Job</th>
+        <th>Favorite Color</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <th>1</th>
+        <td>Cy Ganderton</td>
+        <td>Quality Control Specialist</td>
+        <td>Blue</td>
+      </tr>
+      <tr>
+        <th>2</th>
+        <td>Hart Hagerty</td>
+        <td>Desktop Support Technician</td>
+        <td>Purple</td>
+      </tr>
+      <tr>
+        <th>3</th>
+        <td>Brice Swyre</td>
+        <td>Tax Accountant</td>
+        <td>Red</td>
+      </tr>
+    </tbody>
+  </table>
+</div>
         </div>
     );
 };
