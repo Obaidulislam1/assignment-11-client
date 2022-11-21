c5669cf7d1d9f5493b1e2496f9490f2ebd3e32ce
obaidul islam1 <mursalinislam537215@gmail.com>
2022-11-22 00:21:50 +0600

    loding problem solve

--- src/Component/HeaderItem.js
+++ src/Component/HeaderItem.js
@@ -1,12 +1,22 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Link } from 'react-router-dom';
+import { AuthContext } from '../AuthProvider/AuthProvider';
 
 const HeaderItem = () => {
+    const { user } = useContext(AuthContext)
     return (
         <>
             <li><Link to='/'>Home</Link></li>
-            <li><Link to='/login'>Login</Link></li>
-            <li><Link to='/userReview'>Review</Link></li>
+            {
+                user?.email ?
+                <>
+                <li><Link>Log Out</Link></li>
+                <li><Link to='/userReview'>Review</Link></li>
+                </>
+                :
+                <li><Link to='/login'>Login</Link></li>
+            }
+            
         </>
     );
 };
